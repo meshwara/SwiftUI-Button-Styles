d2526bfe812094fd5f950b660437d52029b992bc
D. Prameswara <meshwara@gmail.com>
2023-01-15 07:03:24 +0700

    Initial Commit

---
+++ ButtonStyles/ButtonStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStylesApp.swift
+//  ButtonStyles
+//
+//  Created by D. Prameswara on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
